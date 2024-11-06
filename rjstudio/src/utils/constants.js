8c583c6290845c2bdfa78650e84928cb51f18524
himmatprabhkar <himmatprabhkar@gmail.com>
2024-11-07 05:00:15 +0530

    change url backend in frontend

--- rjstudio/src/utils/constants.js
+++ rjstudio/src/utils/constants.js
@@ -1,10 +1,10 @@
 export const Constants = {
   URL: {
-    base: 'http://localhost:4000/api/',
-    getPriceAndSize: 'http://localhost:4000/api/get-frame-sizes',
-    getMotifBorder: 'http://localhost:4000/api/get-motif-border-sizes',
-    createOrder: 'http://localhost:4000/api/create-orders',
-    getUserDetails: 'http://localhost:4000/api/user',
+    base: 'https://vercel-deployment-server-7q7mp9v9n-himmats-projects.vercel.app/api/',
+    getPriceAndSize: 'https://vercel-deployment-server-7q7mp9v9n-himmats-projects.vercel.app/api/get-frame-sizes',
+    getMotifBorder: 'https://vercel-deployment-server-7q7mp9v9n-himmats-projects.vercel.app/api/get-motif-border-sizes',
+    createOrder: 'https://vercel-deployment-server-7q7mp9v9n-himmats-projects.vercel.app/api/create-orders',
+    getUserDetails: 'https://vercel-deployment-server-7q7mp9v9n-himmats-projects.vercel.app/api/user',
     // base: "https://rjstudio-hx94.vercel.app/api/",
     login: 'login',
     register: 'register',
