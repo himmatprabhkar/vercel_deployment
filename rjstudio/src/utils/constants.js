fbf52534a5e6435c8e1008fd0b5dd40e517e427f
himmatprabhkar <himmatprabhkar@gmail.com>
2024-11-07 05:50:00 +0530

    change url frontend

--- rjstudio/src/utils/constants.js
+++ rjstudio/src/utils/constants.js
@@ -1,10 +1,10 @@
 export const Constants = {
   URL: {
-    base: 'https://vercel-deployment-server-7q7mp9v9n-himmats-projects.vercel.app/api/',
-    getPriceAndSize: 'https://vercel-deployment-server-7q7mp9v9n-himmats-projects.vercel.app/api/get-frame-sizes',
-    getMotifBorder: 'https://vercel-deployment-server-7q7mp9v9n-himmats-projects.vercel.app/api/get-motif-border-sizes',
-    createOrder: 'https://vercel-deployment-server-7q7mp9v9n-himmats-projects.vercel.app/api/create-orders',
-    getUserDetails: 'https://vercel-deployment-server-7q7mp9v9n-himmats-projects.vercel.app/api/user',
+    base: 'https://vercel-deployment-server-ruddy.vercel.app/api/',
+    getPriceAndSize: 'https://vercel-deployment-server-ruddy.vercel.app/api/get-frame-sizes',
+    getMotifBorder: 'https://vercel-deployment-server-ruddy.vercel.app/api/get-motif-border-sizes',
+    createOrder: 'https://vercel-deployment-server-ruddy.vercel.app/api/create-orders',
+    getUserDetails: 'https://vercel-deployment-server-ruddy.vercel.app/api/user',
     // base: "https://rjstudio-hx94.vercel.app/api/",
     login: 'login',
     register: 'register',
