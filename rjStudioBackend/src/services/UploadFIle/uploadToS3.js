d9f7e409c7fdf2e9e22f272589985cebf9c11021
himmatprabhkar <himmatprabhkar@gmail.com>
2024-11-07 22:48:13 +0530

    change url debug

--- rjStudioBackend/src/services/UploadFIle/uploadToS3.js
+++ rjStudioBackend/src/services/UploadFIle/uploadToS3.js
@@ -10,6 +10,8 @@ AWS.config.region = process.env.REGION;
 
 const s3 = new AWS.S3();
 
+console.log("process.env.AWS_BUCKET_NAMEprocess.env.AWS_BUCKET_NAME", process.env.AWS_BUCKET_NAME);
+
 function uploadImageToS3(fileBuffer, fileName, contentType) {
   const params = {
     Bucket: process.env.AWS_BUCKET_NAME,
@@ -18,6 +20,8 @@ function uploadImageToS3(fileBuffer, fileName, contentType) {
     ContentType: contentType,
     ACL: "public-read",
   };
+
+  console.log('paramsparamsparamsparams', params);
 
   return new Promise((resolve, reject) => {
     s3.upload(params, (err, data) => {
